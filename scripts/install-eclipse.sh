279074e069e7d884d6ceb00850274ac05f8d6995
daileyo <dale13@gmail.com>
2018-05-14 09:14:06 -0700

    Starting to add multiple eclipse options

--- scripts/install-eclipse.sh
+++ scripts/install-eclipse.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-
+typeset -A JDK_LIST
 ECLIPSE_TAR=eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz
+ECLIPSE_LIST=(
+    [oxygen]="http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/oxygen/3a/eclipse-jee-oxygen-3a-linux-gtk-x86_64.tar.gz"
+    [neon]="http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/neon/3/eclipse-jee-neon-3-linux-gtk-x86_64.tar.gz"
+    [mars]="http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/mars/2/eclipse-jee-mars-2-linux-gtk-x86_64.tar.gz"
+)
 
 mkdir -p /opt/eclipse/oxygen
 
-curl -L http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/oxygen/3a/$ECLIPSE_TAR --output $ECLIPSE_TAR -#
+printf "   Downloading Oxygen\n" && curl -L http://mirror.csclub.uwaterloo.ca/eclipse/technology/epp/downloads/release/oxygen/3a/$ECLIPSE_TAR --output $ECLIPSE_TAR -s
 tar -xzf $ECLIPSE_TAR && mv eclipse/* /opt/eclipse/oxygen
 
 alternatives --install /usr/bin/eclipse eclipse /opt/eclipse/oxygen/eclipse 2
