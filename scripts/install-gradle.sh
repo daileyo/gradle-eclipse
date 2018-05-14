e37541efcc355c4d4d411bfa59756596ebf009a9
daileyo <dale13@gmail.com>
2018-05-13 22:08:38 -0700

    Setup parallell download and extraction.

--- scripts/install-gradle.sh
+++ scripts/install-gradle.sh
@@ -10,19 +10,27 @@ i=3
 for ver in $GRADLE_LIST; do 
     mkdir -p /opt/gradle/$ver
     GRADLE_ZIP=gradle-${ver}-all.zip
-    curl -L https://services.gradle.org/distributions/$GRADLE_ZIP --output $GRADLE_ZIP -#
+    printf "   Downloading Gradle ${ver}\n" && curl -L https://services.gradle.org/distributions/$GRADLE_ZIP --output $GRADLE_ZIP -s &
+done
+wait
+for ver in $GRADLE_LIST; do
+    GRADLE_ZIP=gradle-${ver}-all.zip
     unzip -q $GRADLE_ZIP -d .
     mv gradle-${ver}/* /opt/gradle/$ver
-
+done
+wait
+for ver in $GRADLE_LIST; do
+    GRADLE_ZIP=gradle-${ver}-all.zip
     alternatives --install /usr/bin/gradle gradle /opt/gradle/$ver/bin/gradle $i
     i=$((i+1))
     if [ "$GRADLE_VERSION" == "$ver" ]; then
         alternatives --set gradle /opt/gradle/$ver/bin/gradle
     fi
+
     rm -f $GRADLE_ZIP
     rm -rf ${GRADLE_ZIP%-all*}
 done
-
+wait
 sed -i "1iexport GRADLE_HOME=${GRADLE_HOME}" /etc/bashrc
 
 
